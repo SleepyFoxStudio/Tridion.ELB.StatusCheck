0a1e5ec840b6c964f84d6cf571743844d0e4420f
spiderbike <spiderbikejunk@hotmail.co.uk>
2018-03-06 18:25:41 +0000

    Add Regex Options, to choose which services to monitor

--- Tridion.Aws.Status/tridionStatus.ashx.cs
+++ Tridion.Aws.Status/tridionStatus.ashx.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.ServiceProcess;
 using System.Web;
+using System.Web.Configuration;
 
 namespace Tridion.Aws.Status
 {
@@ -19,19 +21,40 @@ namespace Tridion.Aws.Status
 
             context.Response.ContentType = "text/plain";
             context.Response.Write(Environment.MachineName + "\r\n");
+
+
+            if (!bool.TryParse(WebConfigurationManager.AppSettings["active"], out var activeMode))
+            {
+
+                context.Response.StatusCode = 500;
+            }
+
+            if (!activeMode)
+            {
+                context.Response.Write("*************************************************************************\r\n");
+                context.Response.Write("*                                                                       *\r\n");
+                context.Response.Write("*                                                                       *\r\n");
+                context.Response.Write("*                      Services are not monitored                       *\r\n");
+                context.Response.Write("*                      active mode is set false in config               *\r\n");
+                context.Response.Write("*                                                                       *\r\n");
+                context.Response.Write("*                                                                       *\r\n");
+                context.Response.Write("*************************************************************************\r\n");
+                context.Response.StatusCode = 200;
+            }
+
             var svcList = s.GetListOfServices();
-            bool hasError = false;
+            var hasError = false;
             foreach (var service in svcList)
             {
 
-                if (service.Value.Status != ServiceControllerStatus.Running &&
-                    service.Value.StartType != ServiceStartMode.Disabled)
+                if (activeMode && service.Value.Status != ServiceControllerStatus.Running &&
+                    service.Value.StartType == ServiceStartMode.Automatic)
                 {
                     hasError = true;
                 }
                 context.Response.Write(service.Value.DisplayName + ":" + service.Value.Status + Environment.NewLine);
-
             }
+            context.Response.Write(hasError ? "Error" : "OK");
             context.Response.StatusCode = hasError ? 500 : 200;
 
         }
